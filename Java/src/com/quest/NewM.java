66cbc6d6406fa9c8096f7919c0188b09ef0267b4
visha <visha@VISHAK>
2022-03-23 10:26:03 +0530

    Java Program

---
+++ Java/src/com/quest/NewM.java
@@ -0,0 +1,11 @@
+package com.quest;
+
+public class NewM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey! How do you do......");
+
+	}
+
+}
